a4597c6d2f9aef2cf0c4811489615b8b6e4a18ba
TimeJumper1 <benassai.stefano.1@gmail.com>
2022-01-16 16:29:29 +0100

    QOL upgrades

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -12,10 +12,17 @@
                 <li v-else>Titolo:{{ details.name }}</li>
                 <li v-if="details.original_title">Titolo originale:{{ details.original_title }}</li>
                 <li v-else>Titolo originale:{{ details.original_name}}</li>
-                <li>
+                <li v-if="flags.includes(details.original_language)">
+                    <!-- aggiungere v-if per controllo  -->
                     <div><img class="flag" :src="require(`../assets/img/${details.original_language}.png`)" :alt="details.original_language"></div>
                 </li>
-                <li>Voto: <span v-html="star"></span> </li>
+                <li v-else>{{ details.original_language }}</li>
+                <!-- <li>Voto: <span v-html="star"></span> </li> -->
+                <li>Voto:<span v-for="voto  in 5" :key="voto"  >
+                    <span v-if="star >= voto"><i class="fas fa-star"></i></span>
+                    <span v-else><i class="far fa-star"></i></span>
+                    </span>
+                </li>
             
             </ul>
         </div>
@@ -30,35 +37,40 @@ export default {
     },
     data: function(){
         return{
-           star: this.details.vote_average/2 
+           star: parseInt(this.details.vote_average/2) ,
+           flags: ['de', 'en' , 'es' , 'fr', 'gre' ,'it' ,'ja' , 'ko' , 'ru' , 'zh'],
+          
         }
         
     },
 
     methods: {
         // trasformare il voto in stelle
-        starConverter: function(){
-            if(this.star === 0){
-                this.star = `<span><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i></span>`
-            }else if(0 < this.star && this.star<= 1){
-                this.star = `<span><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i></span>`
-            }else if(1 < this.star && this.star<= 2){
-                this.star = `<span><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i></span>`
-            }else if (2<this.star && this.star<=3){
-                this.star = `<span><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i></span>`
-            }else if (3<this.star && this.star<=4){
-                this.star = `<span><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></span>`
-            }else if (4<this.star && this.star<=5){
-                this.star = `<span><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></span>`
-            }
-            return this.star
-        }
+        // starConverter: function(){
+        //     if(this.star === 0){
+        //         this.star = `<span><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i></span>`
+        //     }else if(0 < this.star && this.star<= 1){
+        //         this.star = `<span><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i></span>`
+        //     }else if(1 < this.star && this.star<= 2){
+        //         this.star = `<span><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i></span>`
+        //     }else if (2<this.star && this.star<=3){
+        //         this.star = `<span><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i></span>`
+        //     }else if (3<this.star && this.star<=4){
+        //         this.star = `<span><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></span>`
+        //     }else if (4<this.star && this.star<=5){
+        //         this.star = `<span><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></span>`
+        //     }
+        //     return this.star
+        // },
+        
 
     },
     // inizializzare la funzione di conversione voto
     created: function() {
-        this.starConverter()
-    }
+        // this.starConverter()
+        
+    },
+    
 }
 </script>
 
@@ -74,11 +86,11 @@ export default {
         min-height: 513px; 
     }
     // elementi hover
-    :hover +.poster{
+    &:hover .poster{
         display: none;
             
     }
-    :hover +.info{
+    &:hover .info{
         display: block;
     }
     .poster{
